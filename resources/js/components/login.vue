31b5f84c3b9891ed56676578bfef4bf13625c594
rdsubhashana1994 <59592282+rdsubhashana1994@users.noreply.github.com>
2020-02-03 21:47:47 +0530

    clear welcome.blade page

---
+++ resources/js/components/login.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <form>
+            <div class="form-group">
+                <label for="formGroupExampleInput">Full Name</label>
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input">
+            </div>
+            <div class="form-group">
+                <label for="formGroupExampleInput2">NIC Number</label>
+                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+            </div>
+            <div class="form-group">
+                <label for="formGroupExampleInput3">Occupation</label>
+                <input type="text" class="form-control" id="formGroupExampleInput3" placeholder="Example input">
+            </div>
+            <div class="form-group">
+                <label for="formGroupExampleInput4">Telephone</label>
+                <input type="text" class="form-control" id="formGroupExampleInput4" placeholder="Another input">
+            </div>
+            <div class="form-group">
+                <label for="formGroupExampleInput5">Email</label>
+                <input type="text" class="form-control" id="formGroupExampleInput5" placeholder="Another input">
+            </div>
+            <button type="submit" class="btn btn-primary">Register</button>
+        </form>
+    </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+
+</script>
